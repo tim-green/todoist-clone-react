85a82004523f59dfc673a5307d727b8dbd36dd28
Tim <github@timgreen.ws>
2019-12-29 22:19:22 +1100

    constant for dark mode added

--- src/App.js
+++ src/App.js
@@ -2,3 +2,7 @@ import React, { useState } from 'react';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 import { ProjectsProvider, SelectedProjectProvider } from './context';
+
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
+
